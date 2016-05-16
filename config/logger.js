a1a1ea4d00740b9020b4e3875a3deaa81d16037a
Inumedia <inumedia@inumedia.net>
2016-05-16 00:17:50 -0400

    Generating ActionHero base

---
+++ config/logger.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+var cluster = require('cluster');
+
+exports['default'] = {
+  logger: function(api){
+    var logger = {transports: []};
+
+    // console logger
+    if(cluster.isMaster){
+      logger.transports.push(function(api, winston){
+        return new (winston.transports.Console)({
+          colorize: true,
+          level: 'info',
+          timestamp: true,
+        });
+      });
+    }
+
+    // file logger
+    if(api.config.general.paths.log.length === 1){
+      var logDirectory = api.config.general.paths.log[0];
+      try{
+        fs.mkdirSync(logDirectory);
+      }catch(e){
+        if(e.code !== 'EEXIST'){
+          throw(new Error('Cannot create log directory @ ' + logDirectory));
+        }
+      }
+    }
+
+    logger.transports.push(function(api, winston){
+      return new (winston.transports.File)({
+        filename: api.config.general.paths.log[0] + '/' + api.pids.title + '.log',
+        level: 'info',
+        timestamp: true
+      });
+    });
+
+    // the maximum length of param to log (we will truncate)
+    logger.maxLogStringLength = 100;
+
+    // you can optionally set custom log levels
+    // logger.levels = {good: 0, bad: 1};
+
+    // you can optionally set custom log colors
+    // logger.colors = {good: 'blue', bad: 'red'};
+
+    return logger;
+  }
+};
+
+exports.test = {
+  logger: function(api){
+    return {
+      transports: null
+    };
+  }
+};
---
+++ config/servers/socket.js
@@ -0,0 +1,37 @@
+exports['default'] = {
+  servers: {
+    socket: function(api){
+      return {
+        enabled: false,
+        // TCP or TLS?
+        secure: false,
+        // Passed to tls.createServer if secure=true. Should contain SSL certificates
+        serverOptions: {},
+        // Port or Socket
+        port: 5000,
+        // Which IP to listen on (use 0.0.0.0 for all)
+        bindIP: '0.0.0.0',
+        // Enable TCP KeepAlive pings on each connection?
+        setKeepAlive: false,
+        // Delimiter string for incoming messages
+        delimiter: '\n',
+        // Maximum incoming message string length in Bytes (use 0 for Infinite)
+        maxDataLength: 0,
+        // What message to send down to a client who requests a `quit`
+        goodbyeMessage: 'Bye!'
+      };
+    }
+  }
+};
+
+exports.test = {
+  servers: {
+    socket: function(api){
+      return {
+        enabled: true,
+        port: 5001,
+        secure: false
+      };
+    }
+  }
+};
