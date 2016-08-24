06bb1ac11e4af6dec757a2cbb4cdb55ec7fbbaf5
Inumedia <inumedia@inumedia.net>
2016-08-23 23:20:18 -0400

    Adding API call to get list of items from entire server

--- src/controllers/api/fm.js
+++ src/controllers/api/fm.js
@@ -294,4 +294,92 @@ router.get('/world/:worldId/room/:roomId/items', async (req, res, next) => {
   }
 })
 
+API.registerCall(
+  '/api/fm/world/:worldId/rooms/items',
+  'Gets a list of items in a specific room on a specific world.',
+  [
+    API.createParameter(':worldId', 'number', 'The ID of the world. (0 = Scania, 1 = Windia, 2 = Bera, 3 = Khroa, 4 = MYBCKN, 5 = GRAZED)'),
+    API.createParameter(':roomId', 'number', 'The ID of the room. (1-22)')
+  ],
+  [
+    {
+      cash: {cash:true},
+      equip: {'accountSharable':null,'attack':null,'attackSpeed':null,'bdR':null,'bossReward':null,'charismaEXP':null,'charmEXP':null,'craftEXP':null,'durability':null,'equipTradeBlock':null,'exItem':null,'imdR':null,'incACC':null,'incCraft':null,'incDEX':null,'incEVA':null,'incINT':null,'incJump':null,'incLUK':null,'incMAD':null,'incMDD':null,'incMHP':null,'incMMP':null,'incPAD':null,'incPDD':null,'incSTR':null,'incSpeed':null,'islot':'MaPn','noPotential':null,'reqDEX':0,'reqINT':0,'reqJob':0,'reqJob2':null,'reqLUK':0,'reqLevel':0,'reqPOP':null,'reqSTR':0,'reqSpecJob':null,'senseEXP':null,'superiorEqp':null,'tradeAvailable':null,'tradeBlock':null,'tuc':null,'unchangeable':null,'vslot':'MaPn','willEXP':null},
+      icon: {
+        icon: 'base64/png',
+        iconRaw: 'base64/png'
+      },
+      shop: {'monsterBook':false,'notSale':false,'price':100},
+      chair: {'recoveryHP':100,'recoveryMP':50,'reqLevel':0},
+      description: 'Some really awesome item',
+      ItemId: 0,
+      name: 'Pro Item',
+      slot: 0,
+      acc: 0,
+      avoid: 0,
+      battleModeAtt: 0,
+      bossDmg: 0,
+      bundle: 0,
+      category: 0,
+      channel: 0,
+      characterName: 0,
+      createdAt: 0,
+      creator: 'SomeCoolGuy123456789',
+      dex: 0,
+      diligence: 0,
+      durability: 0,
+      expireTime: 0,
+      growth: 0,
+      hammerApplied: 0,
+      ignoreDef: 0,
+      intelligence: 0,
+      isIdentified: 0,
+      jump: 0,
+      luk: 0,
+      matk: 0,
+      mdef: 0,
+      mhp: 0,
+      mmp: 0,
+      nebulite: 0,
+      numberOfEnhancements: 0,
+      numberOfPlusses: 0,
+      only: 0,
+      potentials: [
+        {
+          'Message': '#prop% chance to ignore #ignoreDAM damage when attacked.',
+          'Modifiers':[{'Item1':'prop','Item2':'30'},{'Item1':'ignoreDAM','Item2':'51'}],
+          'OptionType':20,
+          'id':20353,
+          'target':'potential1',
+          'line':'30% chance to ignore 51 damage when attacked.'
+        }
+      ],
+      price: 0,
+      quantity: 0,
+      rarity: 0,
+      room: 0,
+      server: 0,
+      shopID: 0,
+      shopName: 0,
+      speed: 0,
+      str: 0,
+      untradeable: 0,
+      upgradesAvailable: 0,
+      watk: 0,
+      wdef: 0,
+    }
+  ]
+)
+router.get('/world/:worldId/rooms/items', async (req, res, next) => {
+  try{
+    var worldId = Number(req.params.worldId)
+    var roomId = Number(req.params.roomId)
+    const items = await Item.findAll({server: worldId})
+    res.success(items)
+  }catch(ex){
+    res.status(500).send({error: ex.message || ex, trace: ex.trace || null, stack: ex.stack || null})
+    console.log(ex, ex.stack)
+  }
+})
+
 export default router
