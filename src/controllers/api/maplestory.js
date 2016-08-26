c7f7efa140663e4a199ceffcf5022868cbc34963
Inumedia <inumedia@inumedia.net>
2016-08-25 20:40:42 -0400

    Adding API to get world icons

--- src/controllers/api/maplestory.js
+++ src/controllers/api/maplestory.js
@@ -2,6 +2,7 @@ import express from 'express';
 import r from 'rethinkdb'
 import Promise from 'bluebird'
 import MapleItem from '../../models/mapleitem'
+import World from '../../models/world'
 import API from '../../lib/API'
 
 const router = express.Router();
@@ -44,6 +45,29 @@ router.get('/item/:itemId/iconRaw', async (req, res, next) => {
     if(!item || !item.Icon || !item.Icon.IconRaw) return res.status(404).send('Couldn\'t find an icon for that item.')
 
     var iconData = new Buffer(item.Icon.IconRaw, 'base64')
+    res.set('Content-Type', 'image/png')
+    res.send(iconData)
+  }catch(ex){
+    res.status(500).send({error: ex.message || ex, trace: ex.trace || null, stack: ex.stack || null})
+    console.log(ex, ex.stack)
+  }
+})
+
+API.registerCall(
+  '/api/maplestory/world/:world/icon',
+  'Gets the raw icon of an item',
+  API.createParameter(':itemId', 'number', 'The ID of the item'),
+  'Image/PNG'
+)
+
+router.get('/world/:world/icon', async (req, res, next) => {
+  try{
+    var worldName = req.params.world
+    const world = await World.findFirst({id: worldName.toLowerCase()})
+
+    if(!world || !world.icon) return res.status(404).send('Couldn\'t find an icon for that world.')
+
+    var iconData = new Buffer(world.icon, 'base64')
     res.set('Content-Type', 'image/png')
     res.send(iconData)
   }catch(ex){
