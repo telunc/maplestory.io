05acb4536bff85e4dba6b4b8a3d0ffa17c5c1562
Inumedia <inumedia@inumedia.net>
2016-09-30 00:47:27 -0400

    Pull in redis caching for character data

--- src/controllers/api/character.js
+++ src/controllers/api/character.js
@@ -3,10 +3,24 @@ import r from 'rethinkdb'
 import Promise from 'bluebird'
 import API from '../../lib/API'
 import rp from 'request-promise'
-import cheerio from 'cheerio'
 import Character from '../../models/character'
+import cacheManager from 'cache-manager'
+import redisStore from 'cache-manager-redis'
+import { ENV, PORT, DATADOG_API_KEY, DATADOG_APP_KEY, REDIS_HOST, REDIS_PORT } from '../../environment'
 
 const router = express.Router()
+const redisCache = cacheManager.caching({
+  store: redisStore,
+  host: REDIS_HOST,
+  port: REDIS_PORT,
+  db: 0,
+  ttl: 600
+})
+
+redisCache.store.events.on('redisError', function(error) {
+    // handle error here
+    console.log(error);
+});
 
 API.registerCall(
   '/api/character/:characterName',
@@ -27,52 +41,67 @@ API.registerCall(
 
 router.get('/:characterName', async (req, res, next) => {
   try{
-    let ranking = 'overall'
-    let characterName = req.params.characterName
-    let options = {
+    const ranking = 'overall'
+    const characterName = req.params.characterName
+    const options = {
       uri: `http://maplestory.nexon.net/rankings/${ranking}-ranking/legendary?pageIndex=1&character_name=${characterName}&search=true`,
-      transform: function (body) {
-        return cheerio.load(body);
-      }
     };
 
-    let $ = await rp(options)
+    let rankingListing
 
-    let rankings = $('.ranking-container tbody tr').map((index, ranking) => {
-      let $rank = cheerio.load(ranking)
-      let rankLevel = $rank('td:nth-child(1)').text()
-      let avatar = $rank('img.avatar').attr('src')
-      let characterName = $rank('td:nth-child(3)').text()
-      let world = $rank('.world').attr('title')
-      let characterJob = $rank('img.job').attr('title')
-      let levelMoveInfo = $rank('.level-move').html()
+    let tries = 0
+    while (!(rankingListing = await rp(options)) && ++tries < 5);
 
-      let level, experience, rankMoveInfo
-      [level, experience, rankMoveInfo] = levelMoveInfo.split('\n')
-        .filter((data) => data.trim() !== '')
-        .map((data) => data.trim().replace('<br>', '').replace('<br />', '').replace('(', '').replace(')', ''))
-      level = Number(level)
-      experience = Number(experience)
+    const searchRegex = /<tr>[ \r\n\t]*<td>[0-9]*<\/td>[ \r\n\t]*<td> <img class=\"avatar\"[ \r\n\t]* src=\"([^\"]*)\"><\/td>[ \r\n\t]*<td>(<img src=\"http:\/\/nxcache.nexon.net\/maplestory\/img\/bg\/bg-immigrant.png\"\/><br \/>)*([^<]*)<\/td>[ \r\n\t]*<td><a class=\"([^\"]*)\" href=\"([^\"]*)\" title=\"([^\"]*)\">&nbsp;<\/a><\/td>[ \r\n\t]*<td><img class=\"job\" src=\"([^\"]*)\" alt=\"([^\"]*)\" title=\"[^\"]*\"><\/td>[ \t\r\n]*<td class="level-move">[ \t\r\n]*([0-9]*)<br \/>[ \r\n\t]*\(([0-9]*)\)[ \r\n\t]*<br \/>[ \r\n\t]*<div class=\"rank-([^\"]*)\">([^<]*)<\/div>*/igm
 
-      let rankMovementDirection, rankMovement
-      [rankMovementDirection, rankMovement] = rankMoveInfo.replace('<div class="rank-', '').replace('</div>', '').split('">')
-      rankMovement = Number(rankMovement)
+    let characters = []
+    let match
 
-      return {
-        id: `${characterName}-${ranking}`,
+    while (match = searchRegex.exec(rankingListing)) {
+      const [
+        ,
+        avatar,
+        ,
+        characterName,
+        ,
+        ,
+        world,
+        jobIcon,
+        jobName,
+        level,
+        experience,
+        rankDirection,
+        rankDistance
+      ] = match
+
+      redisCache.set(`ranking-${ranking}-${characterName.toLowerCase()}-`, {
         name: characterName,
-        job: characterJob,
-        ranking: rankLevel,
+        job: jobName,
+        ranking: level,
         world: world,
-        level: level,
         exp: experience,
-        rankMovement: rankMovement,
-        rankDirection: rankMovementDirection,
-        avatar: `/api/character/${characterName}/avatar`
-      }
-    }).get()
+        rankMovement: rankDistance,
+        rankDirection: rankDirection,
+        realAvatar: avatar,
+        got: new Date()
+      })
 
-    res.send(rankings.find((character) => character.id.toLowerCase() == characterName.toLowerCase()))
+      characters.push({
+        name: characterName,
+        job: jobName,
+        ranking: level,
+        world: world,
+        exp: experience,
+        rankMovement: rankDistance,
+        rankDirection: rankDirection,
+        avatar: `/api/character/${characterName}/avatar`,
+        got: new Date()
+      })
+    }
+
+    const characterFound = characters.find((character) => character.name.toLowerCase() == characterName.toLowerCase())
+
+    res.send(characterFound)
   }catch(ex){
     res.status(500).send({error: ex.message || ex, trace: ex.trace || null})
     console.log(ex, ex.stack)
